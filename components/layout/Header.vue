fbd78ececb5736c3e5140dc4f641e4334d95eaad
Yannick Spriessler <87867143+y-neck@users.noreply.github.com>
2025-06-06 11:45:11 +0200

    feat: add header and footer

---
+++ components/layout/Header.vue
@@ -0,0 +1,12 @@
+<template>
+  <header class="my-4 h-fit w-full text-fsize-h2 text-primary">
+    <nav class="flex gap-xl">
+      <NuxtLink to="/" class="nav-item">Home</NuxtLink>
+      <NuxtLink to="/about" class="nav-item">About</NuxtLink>
+    </nav>
+  </header>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
