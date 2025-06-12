c8930a3b837859aa47f3696293388a7cbef5448b
Yannick Spriessler <87867143+y-neck@users.noreply.github.com>
2025-06-12 13:48:57 +0200

    feat: app.vue: add seo meta tags

--- app.vue
+++ app.vue
@@ -1,6 +1,15 @@
 <template>
-    <NuxtLayout>
-      <NuxtPage />
-    </NuxtLayout>
-  </template>
-  
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+</template>
+
+<script>
+useSeoMeta({
+  ogSiteName: "Social Fakeia",
+  ogType: "website",
+  creator: "Yannick Spriessler",
+  robots: "index, follow",
+  ogImage: "" /* Image for sharing */,
+});
+</script>
