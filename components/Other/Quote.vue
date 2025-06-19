c85d663651e4d855f5944468f8d855e742da5c0d
Yannick Spriessler <87867143+y-neck@users.noreply.github.com>
2025-06-19 18:27:24 +0200

    style: Quote.vue Component: font-size

--- components/Other/Quote.vue
+++ components/Other/Quote.vue
@@ -1,8 +1,8 @@
 <template>
   <div
-    class="mx-auto my-4 flex flex-col text-center text-fsize-h2 text-primary italic md:my-8"
+    class="mx-auto my-4 flex flex-col text-center text-primary italic md:my-8"
   >
-    <div><span>«</span><slot /><span>»</span></div>
+    <div class="text-fsize-h3"><span>«</span><slot /><span>»</span></div>
     <LinkPreview class="text-fsize-p text-secondary" :url="props.src">{{
       props.srcName
     }}</LinkPreview>
