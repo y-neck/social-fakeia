4e0f2e444cb5b6d349cdf7f01c438aa6efa6eadd
Yannick Spriessler <87867143+y-neck@users.noreply.github.com>
2025-06-19 14:56:41 +0200

    feat: add Quote.vue Component

---
+++ components/Other/Quote.vue
@@ -0,0 +1,19 @@
+<template>
+  <div
+    class="mx-auto my-4 flex flex-col text-center text-fsize-h2 text-primary italic md:my-8"
+  >
+    <div><span>«</span><slot /><span>»</span></div>
+    <LinkPreview class="text-fsize-p text-secondary" :url="props.src">{{
+      props.srcName
+    }}</LinkPreview>
+  </div>
+</template>
+
+<script setup lang="ts">
+import LinkPreview from "../ui/link-preview/LinkPreview.vue";
+
+const props = defineProps<{
+  src: string;
+  srcName: string;
+}>();
+</script>
