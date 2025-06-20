6ce71c9eecdc18d8f2aa3ef1562ce7b72c69b3c9
Yannick Spriessler <87867143+y-neck@users.noreply.github.com>
2025-06-20 13:09:15 +0200

    feat: definition.vue

---
+++ pages/play/definition.vue
@@ -0,0 +1,171 @@
+<template>
+  <div class="flex flex-col gap-md">
+    <ProgressBar />
+    <h1>Aktuelle Desinformation</h1>
+    <section id="definition-section">
+      <LazyCallout
+        title="«Aktuelle Desinformation» – Häääää?!"
+        icon="clipboard-question"
+      >
+        <p>
+          Vielleicht fragst du dich, was mit diesem Begriff gemeint ist?
+          Wesentlich gängiger ist der Begriff Fake News –
+          <LinkPreview
+            url="https://doi.org/10.1080/0020174X.2018.1508363"
+            class="underline"
+            >Dieser wird jedoch in der Wissenschaft seltener
+            verwendet</LinkPreview
+          >, da er oft als politisches Schlagwort genutzt wird. Auf dieser
+          Website wird deshalb der Begriff der aktuellen Desinformation
+          gebraucht.
+        </p>
+      </LazyCallout>
+      <Quote
+        id="definition-quote"
+        src="https://doi.org/10.1515/9783110679885-008"
+        src-name="Kohring & Zimmermann (2020, S. 1)"
+      >
+        [Desinformation ist] Kommunikation wissentlich und empirisch falscher
+        Informationen zu neuen und relevanten Sachverhalten mit dem Anspruch auf
+        Wahrheit.
+      </Quote>
+      <p>
+        Für den Begriff der (audiovisuellen) Desinformation gibt es verschiedene
+        Definitionen, welche in der Wissenschaft bisher noch nicht einstimmig
+        geklärt sind. Die folgenden Kriterien,
+        <LinkPreview
+          url="https://doi.org/10.1515/9783110679885-008"
+          class="underline"
+        >
+          basierend auf Kohring und Zimmermann (2020),
+        </LinkPreview>
+        helfen, aktuelle Desinformation einzuordnen:
+      </p>
+      <ul id="definition-criteria">
+        <li>
+          <font-awesome-icon icon="newspaper" class="fa-icon" />
+          <span class="list-span">
+            <b>(Visuelle) Kommunikation: </b>Es wird auf visuelle oder andere
+            Weise über ein Thema berichtet. Diese Mitteilung ist potentiell
+            glaubwürdig.
+          </span>
+        </li>
+        <li>
+          <font-awesome-icon icon="fire" class="fa-icon" />
+          <span class="list-span">
+            <b>Aktualitätsbezug: </b>Es handelt sich um ein brisantes und
+            überraschendes Thema, welches grosse Auswirkungen auf die
+            Gesellschaft haben kann.
+          </span>
+        </li>
+        <li>
+          <font-awesome-icon icon="hand-peace" class="fa-icon" />
+          <span class="list-span">
+            <b>Wahrheitsanspruch: </b>Die Nachricht vermittelt, dass ihr Inhalt
+            wahr ist und stellt einen Anspruch an die Zuschauenden, so
+            aufgenommen zu werden.
+          </span>
+        </li>
+        <li>
+          <font-awesome-icon icon="magnifying-glass" class="fa-icon" />
+          <span class="list-span">
+            <b>Unwahrheit: </b>Der Inhalt der Mitteilung ist überprüfbar falsch,
+            löst den eigenen Wahrheitsanspruch nicht ein und ist somit
+            irreführend.
+          </span>
+        </li>
+        <li>
+          <font-awesome-icon icon="square-xmark" class="fa-icon" />
+          <span class="list-span">
+            <b>Unwahrhaftigkeit: </b>Die Produzierenden und Verbreitenden des
+            Inhalts sind sich bewusst, dass die Mitteilung nicht stimmt.
+          </span>
+        </li>
+        <li>
+          <font-awesome-icon icon="user-secret" class="fa-icon" />
+          <span class="list-span">
+            <b>Täuschungsabsicht: </b>Die falschen Informationen werden bewusst
+            und absichtlich verbreitet.
+          </span>
+        </li>
+      </ul>
+    </section>
+    <ClientOnly
+      ><LazyVideoPlayer
+        video-id="mUZj5iC0YoA"
+        script-path="/timestamps/definition-comment.json"
+    /></ClientOnly>
+    <!-- TODO: Video "Was ist Desinformation?" -->
+    <section id="definition-quiz-section">
+      <LazyRadioQuiz interaction-path="definition-quiz" />
+    </section>
+    <LazyButton
+      content="Weiter"
+      textStyling="text-fSize-p"
+      class="mx-auto"
+      @click="navigateTo('/play/current_disinformation')"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import ProgressBar from "~/components/layout/ProgressBar.vue";
+import LazyCallout from "~/components/layout/LazyCallout.vue";
+import LinkPreview from "~/components/ui/link-preview/LinkPreview.vue";
+import Quote from "~/components/Other/Quote.vue";
+import LazyVideoPlayer from "~/components/layout/LazyVideoPlayer.vue";
+import LazyRadioQuiz from "~/components/interaction/LazyRadioQuiz.vue";
+import LazyButton from "~/components/common/LazyButton.vue";
+
+useSeoMeta({
+  title: "Was ist aktuelle Desinformation?" /* Page title */,
+  ogTitle: "Was ist aktuelle Desinformation?" /* Page title without branding */,
+  description: "",
+  ogDescription: "",
+});
+definePageMeta({
+  title: "Definition",
+});
+</script>
+
+<style scoped>
+ul {
+  margin-inline: auto;
+}
+li {
+  list-style-type: disc;
+  margin-inline: 2rem;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  padding-block: calc(var(--spacing) * 2) /* 0.5rem = 8px */;
+  align-items: center;
+}
+
+.list-span {
+  width: calc(3 / 4 * 100%) /* 75% */;
+  text-align: center;
+}
+
+.fa-icon {
+  font-size: var(--text-fsize-h1) /* 2rem = 32px */;
+  color: var(--color-accent);
+}
+
+@media (width >= 48rem /* 768px -> md */) {
+  ul {
+    display: flex;
+    flex-direction: column;
+  }
+  li {
+    display: flex;
+    flex-direction: row;
+    align-items: left;
+    gap: 1rem;
+  }
+  .list-span {
+    width: 100%;
+    text-align: left;
+  }
+}
+</style>
