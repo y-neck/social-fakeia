febfcb20c160506d2f9cfa3ef3479c15f57955f3
Yannick Spriessler <87867143+y-neck@users.noreply.github.com>
2025-06-23 17:32:37 +0200

    feat: current_disinformation.vue

---
+++ pages/play/current_disinformation.vue
@@ -0,0 +1,265 @@
+<template>
+  <div class="flex flex-col gap-md">
+    <ProgressBar />
+    <h1 class="sr-only">Aktuelle Desinformation</h1>
+    <p id="introduction">
+      Seit vor etwa 20 Jahren die Social-Media-Plattformen aufgekommen sind,
+      <LinkPreview class="underline" url="https://doi.org/10.1257/jep.31.2.211"
+        >hat sich die Verbreitung von Informationen stark ins Internet
+        verlagert.</LinkPreview
+      >
+      Die meisten Leute, unter anderem wahrscheinlich auch du,
+      <LinkPreview
+        class="underline"
+        url="https://de.statista.com/statistik/daten/studie/209186/umfrage/zugriff-auf-social-media-via-mobiler-endgeraete-in-ausgewaehlten-laendern/"
+        >informieren sich primär online,</LinkPreview
+      >
+      zunehmend auch auf Social Media. Durch diese Möglichkeit, dass jeder und
+      jede Inhalte online verbreiten kann, gelangen auch immer wieder
+      Falschinformationen oder bewusste Desinformation ins Internet, welche dann
+      weiter verbreitet werden.
+    </p>
+    <section id="spreading" class="flex flex-col gap-md">
+      <h2>Verbreitung</h2>
+      <p>
+        Desinformation ist
+        <LinkPreview class="underline" url="https://doi.org/10.1111/soc4.12724"
+          >kein neues Phänomen</LinkPreview
+        >
+        und wurde schon immer verbreitet. <br />
+        Neu durch die sozialen Plattformen ist die Möglichkeit,
+        <LinkPreview class="underline" url="https://doi.org/10.5167/uzh-209628"
+          >unkontrolliert jegliche Inhalte zu teilen.</LinkPreview
+        >
+        Durch das Ausmass an Inhalten, welche innert kürzester Zeit auf uns
+        einprasseln, ist es ausserdem schwierig, die einzelnen Mitteilungen
+        ausreichend kritisch zu betrachten.
+      </p>
+      <LazyVideoPlayer video-id="" />
+      <!-- TODO: Video current-disinformation_spreading -->
+      <p>
+        Oftmals ist nicht klar,
+        <LinkPreview
+          url="https://doi.org/10.4324/9781003111474-4"
+          class="underline"
+          >von wem die Inhalte im Ursprung stammen.</LinkPreview
+        >
+        Sie werden weiterverbreitet von Nutzenden, welche sich durch die
+        Verbreitung entweder einen Vorteil erhoffen, oder aber
+        <LinkPreview
+          class="underline"
+          url="https://doi.org/10.1108/JPBM-12-2018-2155"
+          >die Desinformation selbst glauben und ihr Umfeld darüber informieren
+          möchten.</LinkPreview
+        >
+      </p>
+      <p>
+        In der Wissenschaft werden vor allem zwei Motivationstreiber für die
+        Produktion und Verbreitung von Desinformation genannt:
+      </p>
+      <ul id="spreading-reasons">
+        <li>
+          <font-awesome-icon icon="money-bill-1-wave" class="fa-icon" />
+          <span class="list-span">
+            <b>Finanzielle Vorteile: </b>Wenn Inhalte auf Social Media viral
+            gehen und vielen Leuten angezeigt werden, kann dies oft finanziellen
+            Gewinn bringen, beispielsweise, indem auf
+            <LinkPreview
+              url="https://onlinelibrary.wiley.com/doi/abs/10.1111/soc4.12724"
+              class="underline"
+              >externe Artikel mit Werbung verlinkt wird.</LinkPreview
+            >
+            Dies wird zum Beispiel durch Medienhäuser angewandt, aber auch durch
+            die Werbeindustrie.
+          </span>
+        </li>
+        <li>
+          <font-awesome-icon icon="landmark" class="fa-icon" />
+          <span class="list-span">
+            <b>Politische Gründe: </b>Durch die Verbreitung von Desinformation
+            können
+            <LinkPreview
+              url="https://link.springer.com/10.1007/978-3-658-25995-2"
+              class="underline"
+              >politische und demokratische Prozesse beeinflusst
+              werden.</LinkPreview
+            >
+            So wurden in vergangenen Fällen Falschinformationen über politische
+            Kandidatinnen und Kanditaten verbreitet oder auch über politische
+            Themen wie den Klimawandel oder Pandemie-Massnahmen.
+          </span>
+        </li>
+      </ul>
+      <p>
+        Die Verbreitung der Desinformation auf den Plattformen geschieht nicht
+        nur durch die Nutzenden selbst, sondern wird
+        <LinkPreview
+          url="https://doi.org/10.1080/10584609.2019.1686094"
+          class="underline"
+          >durch sogenannte Algorithmen auf den Plattformen weiter
+          vorangetrieben.</LinkPreview
+        >
+        Dabei analysieren die Social-Media-Plattformen das Verhalten und die
+        Interessen der Nutzenden, und zeigen ihnen unter anderem Desinformation
+        an,
+        <LinkPreview
+          url="https://link.springer.com/10.1007/978-3-658-25995-2"
+          class="underline"
+        >
+          ihren Interessen entspricht und sie länger auf der Plattform
+          behält.</LinkPreview
+        >
+        Je mehr ähnliche Inhalte sich die Nutzenden anschauen und mit ihnen
+        interagieren, desto eher werden weitere Inhalte ausgespielt. So machen
+        die Plattformen einerseits höhere Umsätze,
+        <LinkPreview
+          url="https://doi.org/10.1108/ITP-10-2020-0699"
+          class="underline"
+          >andererseits tragen sie dazu bei, dass falsche Informationen eine
+          grössere Reichweite hat.</LinkPreview
+        >
+      </p>
+    </section>
+    <section id="production">
+      <h2>Produktion</h2>
+      <p>
+        Desinformation nutzt psychologische Effekte, um zu wirken und verbreitet
+        zu werden. Sie wird besonders
+        <LinkPreview
+          url="https://journals.ala.org/index.php/ltr/article/view/6497"
+          class="underline"
+          >emotional und provokativ gestaltet,</LinkPreview
+        >, beispielsweise indem wütend oder enttäuscht über ein Thema berichtet
+        wird. Dadurch soll die Falschinformation
+        <LinkPreview url="https://doi.org/10.22572/mi.26.2.2" class="underline"
+          >möglichst viel Aufmerksamkeit erregen,</LinkPreview
+        >
+        was wiederum ihre Verbreitung fördert. Durch das Teilen von extremen
+        Meinungen auf emotionale Art werden
+        <LinkPreview
+          url="https://doi.org/10.1515/9783110679885-008"
+          class="underline"
+        >
+          Erwartungen geweckt und Orientierung geboten.</LinkPreview
+        >
+      </p>
+      <LazyAudioPlayer src="1835715834874405" />
+      <p>
+        <LinkPreview
+          url="https://doi.org/10.1108/JPBM-12-2018-2155"
+          class="underline"
+          >Desinformation muss genügend glaubhaft gestaltet sein,</LinkPreview
+        >
+        damit sie als echt wahrgenommen wird. So werden solche Informationen
+        <LinkPreview url="https://doi.org/10.1111/soc4.12724" class="underline"
+          >oft an echte Nachrichten angelehnt</LinkPreview
+        >
+        und zeichnen sich
+        <LinkPreview
+          url="https://web.archive.org/web/20250317104650/https://opendata.uni-halle.de/bitstream/1981185920/117489/1/GrujicNando_Warnhinweise%20bei%20Fake-News%20als%20Strategie%20gegen%20Online-Desinformation.pdf"
+          class="underline"
+          >durch reisserische Überschriften oder fehlende Quellenangaben
+          aus.</LinkPreview
+        >
+      </p>
+    </section>
+    <section id="consequences">
+      <h2>Konsequenzen von desinformierenden Inhalten</h2>
+      <p>
+        Negative Auswirkungen von Desinformation sind nur zu befürchten,
+        <LinkPreview
+          url="https://doi.org/10.1080/15205436.2020.1716983"
+          class="underline"
+          >wenn sie für glaubwürdig erachtet und weiterverbreitet
+          wird.</LinkPreview
+        >
+        Informationen, welche nicht als wahr empfunden werden, werden in den
+        wenigsten Fällen weiterverbreitet. <br /><br />
+        Werden sie jedoch weiter verbreitet, können sie eine ganze Reihe von
+        Auswirkungen haben: Durch Desinformation können Bürgerinnen und Bürger
+        <LinkPreview url="https://doi.org/10.5167/uzh-209628" class="underline"
+          >möglicherweise in ihrer eigenen Meinungsbildung beeinflusst
+          werden,</LinkPreview
+        >
+        was Folgen für Wahlen und Abstimmungen haben kann. Desinformation führt
+        generell zu einem
+        <LinkPreview
+          url="https://doi.org/10.1177/14614448221141648"
+          class="underline"
+          >tieferen Grundvertrauen in öffentliche Institutionen und erhöht die
+          Schwierigkeit, echte von falschen Inhalten zu unterscheiden.
+        </LinkPreview>
+      </p>
+    </section>
+    <section id="current-disinformation-quiz-section">
+      <LazyCheckboxQuiz interaction-path="current-disinformation-quiz" />
+    </section>
+    <LazyButton
+      content="Weiter"
+      textStyling="text-fSize-p"
+      class="mx-auto"
+      @click="navigateTo('/play/audiovisual_disinformation')"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import ProgressBar from "~/components/layout/ProgressBar.vue";
+import LinkPreview from "~/components/ui/link-preview/LinkPreview.vue";
+import LazyVideoPlayer from "~/components/content/LazyVideoPlayer.vue";
+import LazyAudioPlayer from "~/components/content/LazyAudioPlayer.vue";
+import LazyCheckboxQuiz from "~/components/interaction/LazyCheckboxQuiz.vue";
+import LazyButton from "~/components/common/LazyButton.vue";
+
+useSeoMeta({
+  title: "Aktuelle Desinformation" /* Page title */,
+  ogTitle: "Aktuelle Desinformation" /* Page title without branding */,
+  description: "",
+  ogDescription: "",
+});
+definePageMeta({
+  title: "Aktuelle Desinformation",
+});
+</script>
+
+<style scoped>
+ul {
+  margin-inline: auto;
+}
+li {
+  list-style-type: disc;
+  margin-inline: 2rem;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  padding-block: calc(var(--spacing) * 2) /* 0.5rem = 8px */;
+  align-items: center;
+}
+
+.list-span {
+  width: calc(3 / 4 * 100%) /* 75% */;
+  text-align: center;
+}
+
+.fa-icon {
+  font-size: var(--text-fsize-h1) /* 2rem = 32px */;
+  color: var(--color-accent);
+}
+
+@media (width >= 48rem /* 768px -> md */) {
+  ul {
+    display: flex;
+    flex-direction: column;
+  }
+  li {
+    display: flex;
+    flex-direction: row;
+    align-items: left;
+    gap: 1rem;
+  }
+  .list-span {
+    width: 100%;
+    text-align: left;
+  }
+}
+</style>
