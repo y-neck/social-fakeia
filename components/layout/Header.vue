a455a4cd863297e10526b8fbab81e0236ec2a4f4
Yannick Spriessler <87867143+y-neck@users.noreply.github.com>
2025-07-03 16:29:58 +0200

    feat: header.vue: add logo

--- components/layout/Header.vue
+++ components/layout/Header.vue
@@ -1,6 +1,10 @@
 <template>
   <header class="my-4 h-fit w-full bg-background text-fsize-h2 text-primary">
     <nav class="flex gap-xl">
+      <NuxtLink to="/">
+        <img src="/favicon.ico" alt="Logo Social Fakeia" class="h-8 w-8" />
+      </NuxtLink>
+
       <NuxtLink to="/" class="nav-item">Home</NuxtLink>
       <NuxtLink to="/about" class="nav-item">Projekt</NuxtLink>
     </nav>
